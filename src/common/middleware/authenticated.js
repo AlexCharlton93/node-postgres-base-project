ac115b32dac553a9dd6dac8152fc49f688ce2838
Alex Charlton <alex-charlton@live.co.uk>
2022-07-30 19:27:55 +0100

    Fixed authenticated middleware, updated to use .env config

--- src/common/middleware/authenticated.js
+++ src/common/middleware/authenticated.js
@@ -1,8 +1,8 @@
 import jwt from 'jsonwebtoken';
+import jwt_decode from "jwt-decode";
 import { statusCodes } from '../status-codes';
-import { config } from '../config';
 
-export function verifyToken(req, res, next) {
+export const verifyToken = (req, res, next) => {
 	// eslint-disable-next-line dot-notation
 	const bearerHeader = req.headers['authorization'];
 
@@ -11,16 +11,23 @@ export function verifyToken(req, res, next) {
 
 		const bearerToken = bearer[1];
 
-		jwt.verify(bearerToken, config.secretKey, (err) => {
+		jwt.verify(bearerToken, process.env.APP_JWT_SECRET_KEY, (err) => {
 			if (err) {
-				res.sendStatus(statusCodes.FORBIDDEN);
+				res.status(statusCodes.FORBIDDEN).send({
+					message: 'Please login and try again.'
+				});
 			} else {
 				req.token = bearerToken;
 			}
 		});
 
+		const { userId } = jwt_decode(bearerToken);
+		res.locals.userId = userId;
+
 		next();
 	} else {
-		res.sendStatus(statusCodes.FORBIDDEN);
+		res.status(statusCodes.FORBIDDEN).send({
+			message: 'Please login and try again.'
+		});
 	}
 }
